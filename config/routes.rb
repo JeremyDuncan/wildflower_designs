fc5e3a742cb0ba0e21b0f786494f31b5721477e8
JeremyDuncan <jduncan46@my.devry.edu>
2023-11-17 23:00:52 -0500

    added pages and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  # get 'contacts/new'
+  # get 'contacts/create'
+  resources :contacts, only: [:new, :create]
+
+  get 'static_pages/about_me'
+  get 'static_pages/contact'
+
+  get 'about_me', to: 'static_pages#about_me'
+  get 'contact', to: 'static_pages#contact'
+
   resources :images
   resources :products
   resources :pages
