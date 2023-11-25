9a3f14d45ae786c036a87051615146b3686183c5
JeremyDuncan <jduncan46@my.devry.edu>
2023-11-24 21:58:09 -0500

    updated password reset location

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
     authentication: 'plain',
     enable_starttls_auto: true
   }
-  # config.action_mailer.default_url_options = { host: 'www.wild-flower-designs.com',  protocol: 'https'  }
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'www.wild-flower-designs.com',  protocol: 'https'  }
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
 end
