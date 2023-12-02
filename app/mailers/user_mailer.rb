3102a60a449271e9bff60708f5c1526c403e57df
JeremyDuncan <jduncan46@my.devry.edu>
2023-12-01 22:00:38 -0500

    added cart logic

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,9 +7,12 @@ class UserMailer < ApplicationMailer
     mail(to: 'wildflowerdesigns0923@gmail.com', cc:'jeremy.duncan1984@gmail.com',subject: 'Wildflower Designs Customer Message')
   end
 
-  # def contact_email(contact)
-  #   @contact = contact
-  #   @greeting = " 👋🏻 Hello Crystal, You have a new message from an interested customer."
-  #   mail(to: @contact.email, cc: 'jeremy.duncan1984@gmail.com', subject: 'Wildflower Designs Customer Message')
-  # end
+  def invoice_email(user, cart_items)
+    @user = user
+    @cart_items = cart_items
+    # You can add more variables as needed for the email template
+    mail(to: @user.email, bcc: 'wildflowerdesigns0923@gmail.com', subject: 'Your Invoice from Wildflower Designs')
+  end
+
+
 end
